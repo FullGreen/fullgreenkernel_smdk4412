14a43eafc24bfe1434a625efe7581ec73de8acef
fullgreen <abin030187@naver.com>
2016-08-20 20:59:51 +0900

    Mali400 : Fix build error
    Signed-off-by: fullgreen <abin030187@naver.com>

--- drivers/gpu/mali400/r3p2/ump/linux/ump_kernel_linux.c
+++ drivers/gpu/mali400/r3p2/ump/linux/ump_kernel_linux.c
@@ -27,7 +27,6 @@
 #include "ump_kernel_memory_backend.h"
 #include "ump_kernel_memory_backend_os.h"
 #include "ump_kernel_memory_backend_dedicated.h"
-#include "ump_kernel_license.h"
 
 #include "ump_osk.h"
 #include "ump_ukk.h"
