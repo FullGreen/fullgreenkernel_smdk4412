57f8e6132db1a13610ef8bd63f5b657ff01d57d2
fullgreen <abin030187@naver.com>
2016-10-16 08:23:54 +0900

    c1skt: Merge video/samsung from cyanogenmod
    Signed-off-by: fullgreen <abin030187@naver.com>

--- drivers/video/samsung/mdnie_table_c1m0.h
+++ drivers/video/samsung/mdnie_table_c1m0.h
@@ -4,10 +4,29 @@
 #include "mdnie.h"
 
 
+static const unsigned short tune_cyanogenmod[] = {
+	0x0000, 0x0000, /*BANK 0*/
+	0x0008, 0x0088, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
+	0x0092, 0x0040, /*DE pe*/
+	0x0093, 0x0040, /*DE pf*/
+	0x0094, 0x0040, /*DE pb*/
+	0x0095, 0x0040, /*DE ne*/
+	0x0096, 0x0040, /*DE nf*/
+	0x0097, 0x0040, /*DE nb*/
+	0x0098, 0x1000, /*DE max ratio*/
+	0x0099, 0x0100, /*DE min ratio*/
+	0x00b0, 0x1010, /*CS hg ry*/
+	0x00b1, 0x1010, /*CS hg gc*/
+	0x00b2, 0x1010, /*CS hg bm*/
+	0x00b3, 0x1404, /*CS weight grayTH*/
+	0x00ff, 0x0000, /*Mask Release*/
+	END_SEQ, 0x0000,
+};
+
 static const unsigned short tune_dynamic_gallery[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0080, /*DE pe*/
 	0x0093, 0x0080, /*DE pf*/
@@ -24,7 +43,7 @@ static const unsigned short tune_dynamic_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -38,8 +57,7 @@ static const unsigned short tune_dynamic_gallery[] = {
 
 static const unsigned short tune_dynamic_ui[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0040, /*DE pe*/
 	0x0093, 0x0040, /*DE pf*/
@@ -56,7 +74,7 @@ static const unsigned short tune_dynamic_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -70,8 +88,7 @@ static const unsigned short tune_dynamic_ui[] = {
 
 static const unsigned short tune_dynamic_video[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0080, /*DE pe*/
 	0x0093, 0x0080, /*DE pf*/
@@ -100,7 +117,7 @@ static const unsigned short tune_dynamic_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -114,8 +131,7 @@ static const unsigned short tune_dynamic_video[] = {
 
 static const unsigned short tune_dynamic_vt[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008e,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008e, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0005, /*FA cs1 | de8 dnr4 hdr2 fa1*/
 	0x0039, 0x0080, /*FA dnrWeight*/
 	0x0080, 0x0fff, /*DNR dirTh*/
@@ -137,7 +153,7 @@ static const unsigned short tune_dynamic_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	0*/
-	0x0021, 0x0293, /*CC lut r	16 144*/
+	0x0021, 0x0d93, /*CC lut r	16 144*/
 	0x0022, 0x1aa5, /*CC lut r	32 160*/
 	0x0023, 0x29b7, /*CC lut r	48 176*/
 	0x0024, 0x39c8, /*CC lut r	64 192*/
@@ -151,8 +167,7 @@ static const unsigned short tune_dynamic_vt[] = {
 
 static const unsigned short tune_movie_gallery[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00a0,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00a0, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x00e1, 0xd6ac, /*SCR RrCr*/
 	0x00e2, 0x32ff, /*SCR RgCg*/
@@ -169,7 +184,7 @@ static const unsigned short tune_movie_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -183,8 +198,7 @@ static const unsigned short tune_movie_gallery[] = {
 
 static const unsigned short tune_movie_ui[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00a0,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00a0, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x00e1, 0xd6ac, /*SCR RrCr*/
 	0x00e2, 0x32ff, /*SCR RgCg*/
@@ -201,7 +215,7 @@ static const unsigned short tune_movie_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -215,8 +229,7 @@ static const unsigned short tune_movie_ui[] = {
 
 static const unsigned short tune_movie_video[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00a0,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00a0, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0000, /*DE pe*/
 	0x0093, 0x0000, /*DE pf*/
@@ -243,7 +256,7 @@ static const unsigned short tune_movie_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -257,8 +270,7 @@ static const unsigned short tune_movie_video[] = {
 
 static const unsigned short tune_movie_vt[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ae,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ae, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0005, /*FA cs1 | de8 dnr4 hdr2 fa1*/
 	0x0039, 0x0080, /*FA dnrWeight*/
 	0x0080, 0x0fff, /*DNR dirTh*/
@@ -292,7 +304,7 @@ static const unsigned short tune_movie_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -306,8 +318,7 @@ static const unsigned short tune_movie_vt[] = {
 
 static const unsigned short tune_standard_gallery[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0060, /*DE pe*/
 	0x0093, 0x0060, /*DE pf*/
@@ -324,7 +335,7 @@ static const unsigned short tune_standard_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -338,8 +349,7 @@ static const unsigned short tune_standard_gallery[] = {
 
 static const unsigned short tune_standard_ui[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0020, /*DE pe*/
 	0x0093, 0x0020, /*DE pf*/
@@ -356,7 +366,7 @@ static const unsigned short tune_standard_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -370,8 +380,7 @@ static const unsigned short tune_standard_ui[] = {
 
 static const unsigned short tune_standard_video[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0060, /*DE pe*/
 	0x0093, 0x0060, /*DE pf*/
@@ -400,7 +409,7 @@ static const unsigned short tune_standard_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -414,8 +423,7 @@ static const unsigned short tune_standard_video[] = {
 
 static const unsigned short tune_standard_vt[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x008e,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x008e, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0005, /*FA cs1 | de8 dnr4 hdr2 fa1*/
 	0x0039, 0x0080, /*FA dnrWeight*/
 	0x0080, 0x0fff, /*DNR dirTh*/
@@ -437,7 +445,7 @@ static const unsigned short tune_standard_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290,	/*CC lut r	16 144*/
+	0x0021, 0x1090,	/*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -451,8 +459,7 @@ static const unsigned short tune_standard_vt[] = {
 
 static const unsigned short tune_natural_gallery[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ac,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ac, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0060, /*DE pe*/
 	0x0093, 0x0060, /*DE pf*/
@@ -481,7 +488,7 @@ static const unsigned short tune_natural_gallery[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -495,8 +502,7 @@ static const unsigned short tune_natural_gallery[] = {
 
 static const unsigned short tune_natural_ui[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ac,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ac, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0020, /*DE pe*/
 	0x0093, 0x0020, /*DE pf*/
@@ -525,7 +531,7 @@ static const unsigned short tune_natural_ui[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -539,8 +545,7 @@ static const unsigned short tune_natural_ui[] = {
 
 static const unsigned short tune_natural_video[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ac,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ac, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0060, /*DE pe*/
 	0x0093, 0x0060, /*DE pf*/
@@ -569,7 +574,7 @@ static const unsigned short tune_natural_video[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -583,8 +588,7 @@ static const unsigned short tune_natural_video[] = {
 
 static const unsigned short tune_natural_vt[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ae,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ae, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0005, /*FA cs1 | de8 dnr4 hdr2 fa1*/
 	0x0039, 0x0080, /*FA dnrWeight*/
 	0x0080, 0x0fff, /*DNR dirTh*/
@@ -618,7 +622,7 @@ static const unsigned short tune_natural_vt[] = {
 	0x0000, 0x0001, /*BANK 1*/
 	0x001f, 0x0080, /*CC chsel strength*/
 	0x0020, 0x0000, /*CC lut r	 0*/
-	0x0021, 0x0290, /*CC lut r	16 144*/
+	0x0021, 0x1090, /*CC lut r	16 144*/
 	0x0022, 0x20a0, /*CC lut r	32 160*/
 	0x0023, 0x30b0, /*CC lut r	48 176*/
 	0x0024, 0x40c0, /*CC lut r	64 192*/
@@ -632,8 +636,7 @@ static const unsigned short tune_natural_vt[] = {
 
 static const unsigned short tune_camera[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x000c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x000c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0060, /*DE pe*/
 	0x0093, 0x0060, /*DE pf*/
@@ -653,8 +656,7 @@ static const unsigned short tune_camera[] = {
 
 static const unsigned short tune_camera_outdoor[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x040c,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x040c, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0030, 0x0000, /*FA cs1 de8 hdr2 fa1*/
 	0x0092, 0x0060, /*DE pe*/
 	0x0093, 0x0060, /*DE pf*/
@@ -677,8 +679,7 @@ static const unsigned short tune_camera_outdoor[] = {
 
 static const unsigned short tune_cold[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ec,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ec, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0000, 0x0001, /*BANK 1*/
 	0x0001, 0x0064, /*MCM 10000K*/
 	0x0009, 0xa08b, /*MCM 5cb 1cr W*/
@@ -689,8 +690,7 @@ static const unsigned short tune_cold[] = {
 
 static const unsigned short tune_cold_outdoor[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x04ec,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x04ec, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0000, 0x0001, /*BANK 1*/
 	0x0001, 0x0064, /*MCM 10000K*/
 	0x0009, 0xa08b, /*MCM 5cb 1cr W*/
@@ -703,8 +703,7 @@ static const unsigned short tune_cold_outdoor[] = {
 
 static const unsigned short tune_normal_outdoor[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x04ac,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x04ac, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0000, 0x0001, /*BANK 1*/
 	0x00d0, 0x01c0, /*UC y*/
 	0x00d1, 0x01ff, /*UC cs*/
@@ -714,8 +713,7 @@ static const unsigned short tune_normal_outdoor[] = {
 
 static const unsigned short tune_warm[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x00ec,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x00ec, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0000, 0x0001, /*BANK 1*/
 	0x0001, 0x0028, /*MCM 4000K*/
 	0x0007, 0x7878, /*MCM 1cb 2cb W*/
@@ -726,8 +724,7 @@ static const unsigned short tune_warm[] = {
 
 static const unsigned short tune_warm_outdoor[] = {
 	0x0000, 0x0000, /*BANK 0*/
-	0x0008, 0x04ec,
-	/*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
+	0x0008, 0x04ec, /*Dither8 UC4 ABC2 CP1 | CC8 MCM4 SCR2 SCC1 | CS8 DE4 DNR2 HDR1*/
 	0x0000, 0x0001, /*BANK 1*/
 	0x0001, 0x0028, /*MCM 4000K*/
 	0x0007, 0x7878, /*MCM 1cb 2cb W*/
@@ -756,6 +753,7 @@ struct mdnie_tunning_info etc_table[CABC_MAX][OUTDOOR_MAX][TONE_MAX] = {
 struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 	{
 		{
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"DYNAMIC_UI",		tune_dynamic_ui},
 			{"DYNAMIC_VIDEO",	tune_dynamic_video},
 			{"DYNAMIC_VIDEO",	tune_dynamic_video},
@@ -765,6 +763,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"DYNAMIC_GALLERY",	tune_dynamic_gallery},
 			{"DYNAMIC_VT",		tune_dynamic_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"STANDARD_UI",		tune_standard_ui},
 			{"STANDARD_VIDEO",	tune_standard_video},
 			{"STANDARD_VIDEO",	tune_standard_video},
@@ -774,6 +773,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"STANDARD_GALLERY",	tune_standard_gallery},
 			{"STANDARD_VT",		tune_standard_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"NATURAL_UI",		tune_natural_ui},
 			{"NATURAL_VIDEO",	tune_natural_video},
 			{"NATURAL_VIDEO_WARM",	tune_natural_video},
@@ -783,6 +783,7 @@ struct mdnie_tunning_info tunning_table[CABC_MAX][MODE_MAX][SCENARIO_MAX] = {
 			{"NATURAL_GALLERY",	tune_natural_gallery},
 			{"NATURAL_VT",		tune_natural_vt},
 		}, {
+			{"CYANOGENMOD",		tune_cyanogenmod},
 			{"MOVIE_UI",		tune_movie_ui},
 			{"MOVIE_VIDEO",		tune_movie_video},
 			{"MOVIE_VIDEO",		tune_movie_video},
--- drivers/video/samsung/mdnie_tuning_kona.c
+++ drivers/video/samsung/mdnie_tuning_kona.c
@@ -8,22 +8,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
 */
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/errno.h>
-#include <linux/poll.h>
-#include <linux/wait.h>
-#include <linux/fs.h>
-#include <linux/irq.h>
-#include <linux/mm.h>
-#include <linux/device.h>
-#include <linux/platform_device.h>
-#include <linux/ctype.h>
-#include <linux/delay.h>
-#include <linux/lcd.h>
-#include <linux/rtc.h>
-#include <linux/fb.h>
-#include <mach/gpio.h>
 
 #include <linux/errno.h>
 #include <linux/fs.h>
@@ -31,27 +15,25 @@
 #include <linux/uaccess.h>
 #include <linux/magic.h>
 
-#include "mdnie.h"
+#include "s3cfb.h"
+#include "mdnie_kona.h"
 
 #define KFREE(ptr)	do { if (ptr) kfree(ptr); (ptr) = NULL; } while (0)
-#define CONSTANT_F1(x)			(((x << 10) * 23) / 21)
-#define CONSTANT_F2(x)			(((x << 10) * 19) / 18)
-#define CONSTANT_F3(x)			(((x << 10) * 5))
-#define CONSTANT_F4(x)			(((x << 10) * 21) / 8)
-
-#define COLOR_OFFSET_F1(x, y)		(((y << 10) - CONSTANT_F1(x) + (12 << 10)) >> 10)
-#define COLOR_OFFSET_F2(x, y)		(((y << 10) - CONSTANT_F2(x) + (44 << 10)) >> 10)
-#define COLOR_OFFSET_F3(x, y)		(((y << 10) + CONSTANT_F3(x) - (18365 << 10)) >> 10)
-#define COLOR_OFFSET_F4(x, y)		(((y << 10) + CONSTANT_F4(x) - (10814 << 10)) >> 10)
+#define CONSTANT_F1	((23<<10)/21)
+#define CONSTANT_F2	((19<<10)/18)
+#define CONSTANT_F3	(5<<10)
+#define CONSTANT_F4	((21<<10)/8)
 
 int mdnie_calibration(unsigned short x, unsigned short y, int *result)
 {
-	int ret = 0;
+	u8 ret = 0;
 
-	result[1] = COLOR_OFFSET_F1(x, y);
-	result[2] = COLOR_OFFSET_F2(x, y);
-	result[3] = COLOR_OFFSET_F3(x, y);
-	result[4] = COLOR_OFFSET_F4(x, y);
+	result[1] = ((y << 10) - (CONSTANT_F1 * x) + (12 << 10)) >> 10;
+	result[2] = ((y << 10) - (CONSTANT_F2 * x) + (44 << 10)) >> 10;
+	result[3] = ((y << 10) + (CONSTANT_F3 * x) - (18365 << 10)) >> 10;
+	result[4] = ((y << 10) + (CONSTANT_F4 * x) - (10814 << 10)) >> 10;
+
+	pr_info("%d, %d, %d, %d\n", result[1], result[2], result[3], result[4]);
 
 	if (result[1] > 0) {
 		if (result[3] > 0)
@@ -75,9 +57,118 @@ int mdnie_calibration(unsigned short x, unsigned short y, int *result)
 	ret = (ret > 0) ? ret : 1;
 	ret = (ret > 9) ? 1 : ret;
 
-	pr_info("%d, %d, %d, %d, TUNE%d\n", result[1], result[2], result[3], result[4], ret);
-
 	return ret;
+}
+
+static int parse_text(char *src, int len, u16 *buf, char *name)
+{
+	int i, count, ret;
+	int index = 0;
+	char *str_line[100];
+	char *sstart;
+	char *c;
+	unsigned int data1, data2;
+
+	c = src;
+	count = 0;
+	sstart = c;
+
+	for (i = 0; i < len; i++, c++) {
+		char a = *c;
+		if (a == '\r' || a == '\n') {
+			if (c > sstart) {
+				str_line[count] = sstart;
+				count++;
+			}
+			*c = '\0';
+			sstart = c+1;
+		}
+	}
+
+	if (c > sstart) {
+		str_line[count] = sstart;
+		count++;
+	}
+
+	/* printk(KERN_INFO "----------------------------- Total number of lines:%d\n", count); */
+
+	for (i = 0; i < count; i++) {
+		/* printk(KERN_INFO "line:%d, [start]%s[end]\n", i, str_line[i]); */
+		ret = sscanf(str_line[i], "0x%x, 0x%x\n", &data1, &data2);
+		/* printk(KERN_INFO "Result => [0x%2x 0x%4x] %s\n", data1, data2, (ret == 2) ? "Ok" : "Not available"); */
+		if (ret == 2) {
+			buf[index++] = (unsigned short)data1;
+			buf[index++] = (unsigned short)data2;
+		}
+	}
+
+	buf[index] = END_SEQ;
+
+	return index;
+}
+
+int mdnie_txtbuf_to_parsing(char *path, u16 size, u16 *buf, char *name)
+{
+	struct file *filp;
+	char	*dp;
+	long	l;
+	loff_t  pos;
+	int     ret, num;
+	mm_segment_t fs;
+
+	fs = get_fs();
+	set_fs(get_ds());
+
+	if (!path) {
+		pr_err("%s: invalid filepath\n", __func__);
+		goto parse_err;
+	}
+
+	filp = filp_open(path, O_RDONLY, 0);
+
+	if (IS_ERR(filp)) {
+		pr_err("file open error: %s\n", path);
+		goto parse_err;
+	}
+
+	l = filp->f_path.dentry->d_inode->i_size;
+	dp = kmalloc(l, GFP_KERNEL);
+	if (dp == NULL) {
+		pr_err("Out of Memory!\n");
+		filp_close(filp, current->files);
+		goto parse_err;
+	}
+	pos = 0;
+	memset(dp, 0, l);
+	ret = vfs_read(filp, (char __user *)dp, l, &pos);
+
+	if (ret != l) {
+		pr_info("read size = %d, l = %ld, size=%d\n", ret, l, size);
+		l = (l > ret) ? ret : l;
+		if (size < l) {
+			KFREE(dp);
+			filp_close(filp, current->files);
+			goto parse_err;
+		}
+	}
+
+	filp_close(filp, current->files);
+	set_fs(fs);
+
+	num = parse_text(dp, l, buf, name);
+
+	if (!num) {
+		pr_err("Nothing to parse!\n");
+		KFREE(dp);
+		goto parse_err;
+	}
+
+	KFREE(dp);
+
+	return num;
+
+parse_err:
+	return -EPERM;
 }
 
 int mdnie_open_file(const char *path, char **fp)
@@ -96,19 +187,19 @@ int mdnie_open_file(const char *path, char **fp)
 
 	filp = filp_open(path, O_RDONLY, 0);
 	if (IS_ERR(filp)) {
-		pr_err("%s: filp_open skip: %s\n", __func__, path);
+		pr_err("%s: file open err: %s\n", __func__, path);
 		return -EPERM;
 	}
 
 	length = i_size_read(filp->f_path.dentry->d_inode);
 	if (length <= 0) {
-		pr_err("%s: file length is %ld\n", __func__, length);
+		pr_err("%s: file size %ld error\n", __func__, length);
 		return -EPERM;
 	}
 
 	dp = kzalloc(length, GFP_KERNEL);
 	if (dp == NULL) {
-		pr_err("%s: fail to alloc size %ld\n", __func__, length);
+		pr_err("%s: Out of Memory\n", __func__);
 		filp_close(filp, current->files);
 		return -EPERM;
 	}
@@ -139,9 +230,9 @@ int mdnie_check_firmware(const char *path, char *name)
 
 	size = mdnie_open_file(path, &ptr);
 	if (IS_ERR_OR_NULL(ptr) || size <= 0) {
-		pr_err("%s: file open skip %s\n", __func__, path);
+		pr_err("%s: file open fail %s\n", __func__, path);
 		KFREE(ptr);
-		return -EPERM;
+		return 0;
 	}
 
 	ret = (strstr(ptr, name) != NULL) ? 1 : 0;
@@ -151,7 +242,7 @@ int mdnie_check_firmware(const char *path, char *name)
 	return ret;
 }
 
-int mdnie_request_firmware(const char *path, u16 **buf, const char *name)
+int mdnie_request_firmware(const char *path, u16 **buf, char *name)
 {
 	char *token, *ptr = NULL;
 	unsigned short *dp;
@@ -160,14 +251,14 @@ int mdnie_request_firmware(const char *path, u16 **buf, const char *name)
 
 	size = mdnie_open_file(path, &ptr);
 	if (IS_ERR_OR_NULL(ptr) || size <= 0) {
-		pr_err("%s: file open skip %s\n", __func__, path);
+		pr_err("%s: file open fail %s\n", __func__, path);
 		KFREE(ptr);
-		return -EPERM;
+		return ret;
 	}
 
 	dp = kzalloc(size, GFP_KERNEL);
 	if (dp == NULL) {
-		pr_err("%s: fail to alloc size %d\n", __func__, size);
+		pr_err("%s: Out of Memory\n", __func__);
 		KFREE(ptr);
 		return -ENOMEM;
 	}
